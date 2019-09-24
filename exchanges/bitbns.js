33a34dd437fc167ad5f20697344e81848cfb11d3
Jaison Titus <jaisontj92@gmail.com>
2019-09-23 23:32:00 -0700

    Fixed issues with trade book aggregation

--- exchanges/bitbns.js
+++ exchanges/bitbns.js
@@ -11,9 +11,10 @@ const prices = {'INR': {}};
  * Here, amount is the amount in the market currency; 
  */
 function getAvgPrice(amount, priceList) {
-	var count = 0.0
-	var totalPrice = 0
-	priceList.forEach(price => {
+	var count = 0.0;
+	var totalPrice = amount;
+	for (var i = 0; i < priceList.length; i++) {
+		const price = priceList[i];
 		if (amount <= 0) {
 			break;
 		}
@@ -23,35 +24,42 @@ function getAvgPrice(amount, priceList) {
 			amount = amount - amt
 			count = count + price.btc
 		} else {
-			amount = 0
 			count = count + (amount/price.rate)
+			amount = 0
 		}
-	})
+	}
 	if (amount != 0) {
 		return -1;
 	}
-	return amount/count
+	return totalPrice/count
 }
 
 function getPrices(market, amount) {
 	newPrices = {}
 	marketPrices = prices[market]
 	for (var coin in marketPrices) {
-		var avgBuyPrice = marketPrices[coin]['buy']
-		if (marketPrices.hasOwnProperty('buyList')) {
-			avgBuyPrice = getAvgPrice(amount, marketPrice['buyList'])
+		const coinPrice = marketPrices[coin]
+		var avgBuyPrice = coinPrice['buy']
+		if (coinPrice.hasOwnProperty('buyList')) {
+			avgBuyPrice = getAvgPrice(amount, coinPrice['buyList'])
 			if (avgBuyPrice == -1) {
 				console.log(`Skipping ${coin}/${market} since trade book is insufficient to calculate buyprice for amount`)
-				avgBuyPrice = marketPrices[coin]['buy']
+				continue;
 			}
+		} else {
+			console.log(`Skipping ${coin}/${market} since marketPrice has no property 'buyList'`);
+			continue;
 		}
-		var avgSellPrice = marketPrices[coin]['sell']
-		if (marketPrices.hasOwnProperty('sellList')) {
-			avgSellPrice = getAvgPrice(amount, marketPrice['sellList'])
+		var avgSellPrice = coinPrice['sell']
+		if (coinPrice.hasOwnProperty('sellList')) {
+			avgSellPrice = getAvgPrice(amount, coinPrice['sellList'])
 			if (avgSellPrice == -1) {
 				console.log(`Skipping ${coin}/${market} since trade book is insufficient to calculate sellprice for amount`)
-				avgSellPrice = marketPrices[coin]['sell']
+				continue;
 			}
+		} else {
+			console.log(`Skipping ${coin}/${market} since marketPrice has no property 'sellList'`);
+			continue;
 		}
 		newPrices[coin] = {
 			buy: avgBuyPrice,
@@ -61,16 +69,45 @@ function getPrices(market, amount) {
 	return newPrices;
 }
 
+const failedPairs = {
+	'BCHABC/INR': 4,
+	'PHB/INR': 4,
+	'VEN/INR': 4,
+	'SC/INR': 4,
+	'RPX/INR': 4,
+	'RAM/INR': 4,
+	'VEN/INR': 4
+};
+
+function shouldSkipPair(coin, market) {
+	const pair = `${coin}/${market}`;
+	if (!failedPairs.hasOwnProperty(pair)) {
+		return false;
+	}
+	// if this pair returned null data > 3 times, then skip this
+	return failedPairs[pair] > 3;
+}
+
+function trackFailedPair(coin, market) {
+	const pair = `${coin}/${market}`;
+	if (!failedPairs.hasOwnProperty(pair)) {
+		failedPairs[pair] = 0;
+	}
+	failedPairs[pair]++;
+}
 
 function openSocketToOrderBook(coin, market) {
+	if (shouldSkipPair(coin, market)) {
+		return;
+	}
 	const socket = bitbns.getOrderBookSocket(coin, market)
 	socket.on('connect', () => { 
-		console.log(`Connected to socket ${coin}/${market}`)
 		socket.on('news', res => {
 			try {
 				const data = JSON.parse(res)
 				if (data === null) {
 					console.log(`Data Received at ${coin}/${market}: NULL`)
+					trackFailedPair(coin, market);
 					return;
 				}
 				if (data.type == 'tradeList') {
@@ -82,9 +119,16 @@ function openSocketToOrderBook(coin, market) {
 					console.log(`Data Received at ${coin}/${market}`)
 					console.log('JSON parse failed: ')
 					console.log(data)
+					trackFailedPair(coin, market);
 				}
 			} catch (e) {
-				console.log(`Error in the Stream ${coin}/${market}`, e)
+				console.log(`Error in the Stream ${coin}/${market}`)
+				console.log(`${e.name}: ${e.message}`)
+				if (e instanceof SyntaxError) {
+					console.log(`JSON parsing error while parsing: \n`);
+					console.log(res);
+					trackFailedPair(coin, market);
+				} 
 				openSocketToOrderBook(coin, market)
 			}
 		})
